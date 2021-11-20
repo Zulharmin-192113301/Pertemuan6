a5fcb7f133bfec101341fbcd604743e252d37069
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-11-20 14:01:28 +0700

    Pertemuan 6 - Route Array

--- src/App.js
+++ src/App.js
@@ -32,6 +32,22 @@ class App extends Component {
     }}>Logout</button>
 ))
 
+const routes = [{
+  path : '/',
+  exact : true,
+  render : ()=> <div>Ini halaman Home</div>
+},{
+  path : '/news',
+  render : ()=> <div>Ini halaman News</div>
+},{
+  path : '/login',
+  render : ()=> <LoginButton />
+},{
+  path : '/profile',
+  render : ()=> this.state.isAuth ? <div>Ini halaman Profile <br /><LogoutButton /></div> : <Redirect to='/login' />
+}]
+
+
     return (
       <Router>
         <div>
@@ -42,10 +58,11 @@ class App extends Component {
           </ul>
 
         <Switch>
-            <Route path='/' exact render={() => <div>Halaman Home ya</div>} />
-            <Route path='/news' render={() => <div>Kalo ini halaman News</div>} />
-            <Route path='/login' render={()=> <LoginButton /> } />
-            <Route path='/profile' render={()=> this.state.isAuth ? <div>Ini halaman Profile <br /> <LogoutButton /> </div> : <Redirect to='/login' />} />
+        {
+              routes.map((item, index)=>(
+                <Route path={item.path} exact={item.exact} render={item.render} />
+              ))
+            }
         </Switch>
 
         </div>
