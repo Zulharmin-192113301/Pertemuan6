8b2f67f6c1fd872245ba1608a8ecbb6a62860ca7
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-11-20 14:59:04 +0700

    Tugas 6

--- src/App.js
+++ src/App.js
@@ -1,33 +1,111 @@
 import React, {Component} from 'react';
+import './App.css'
+import './css/bootstrap.min.css';
+import PasPhoto from './img/pasphoto.png';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  withRouter,
+  NavLink
+} from 'react-router-dom';
 
 
 class App extends Component {
   constructor(){
-    super();
-    this.myRef = React.createRef();
-  }
-
-  componentDidMount(){
-    this.myRef.current.style.height = '100vh'
-    this.myRef.current.style.width = '100%'
-  }
-
-  setBackground(bgColor, txtColor){
-    this.myRef.current.style.background = bgColor
-    this.myRef.current.style.color = txtColor
+    super()
+    this.state ={
+      isAuth : false
+    }
   }
 
   render() {
+    const LoginButton = withRouter(({history}) => (
+      <button onClick={()=>{
+        this.setState({isAuth : true})
+        history.push('/profile')
+      }}>Login</button>
+  ))
+
+  const LogoutButton = withRouter(({history}) => (
+    <button onClick={()=>{
+      this.setState({isAuth : false})
+      history.push('/login')
+    }}>Logout</button>
+))
+
 
     return (
-      <div ref={this.myRef}>
-      <p>Belajar pengembangan aplikasi web dengan Reactjs dengan topik Router dan Refs</p>
-      <button onClick={this.setBackground.bind(this, 'white', 'black')}> WHITE </button>
-      <button onClick={this.setBackground.bind(this, 'red', 'white')}> RED </button>
-      <button onClick={this.setBackground.bind(this, 'green', 'white')}> GREEN </button>
-      <button onClick={this.setBackground.bind(this, 'blue', 'white')}> BLUE </button>
-  </div>
-  );
+      <Router>
+      <div>
+      <nav className='navbar navbar-expand-sm bg-dark navbar-dark'>
+              <ul className='navbar-nav'>
+        <li className='nav-item'><NavLink exact activeStyle={{fontWeight: 'bold', color: 'red'}} to='/'>Home &nbsp;</NavLink></li>
+        <li className='nav-item'><NavLink activeClassName='active-link' to='/profile'>Profile</NavLink></li>
+        </ul>
+        </nav>
+      
+      
+      <Switch>
+          <Route path='/' exact render={() => <div>Halaman Home ya</div>} />
+          <Route path='/login' render={()=> <LoginButton /> } />
+          
+          <Route path='/profile' render={()=> this.state.isAuth ? 
+          
+          <div>
+            
+            <table class="table table-light">
+  <thead>
+    <tr>
+      <th scope="col">Identitas</th>
+      <th scope="col">Data</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>Pas Photo</th>
+      <td><img width='300px' height='auto' src={PasPhoto} /></td>
+
+    </tr>
+    <tr>
+      <th scope="row">Nama</th>
+      <td>Zulharmin</td>
+
+    </tr>
+    <tr>
+      <th scope="row">Nim</th>
+      <td>192113301</td>
+    </tr>
+
+    <tr>
+      <th scope="row">Jurusan</th>
+      <td>Sistem Informasi</td>
+    </tr>
+
+    <tr>
+      <th scope="row">Kelas</th>
+      <td>Ebiz B Sore</td>
+    </tr>
+
+    <tr>
+      <th scope="row">Semester</th>
+      <td>5 (Lima) </td>
+    </tr>
+
+  </tbody>
+</table>
+
+            <br /><br /> <LogoutButton />  <br /><br /> 
+          
+          </div> : <Redirect to='/login' />} />
+      
+      </Switch>
+      </div>
+      </Router>
+  )
   }
 }
 
